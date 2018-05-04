24a8ddb3a8eba637c64d6037c9c73c40e16bfcec
Jonathan Reisdorf <fishlevel@crossnode.org>
2018-05-04 18:10:51 +0100

    new navigation

--- www/js/index.js
+++ www/js/index.js
@@ -2,48 +2,13 @@ class BB10Browser {
   constructor(url) {
     this.browser = window.inAppBrowserXwalk.open(url, {
       openHidden: false,
-      height: this._getContentHeight()
+      navigationHeight: 40
     });
 
     this.attachEvents();
   }
 
-  _getViewportHeight() {
-    return window.innerHeight * window.devicePixelRatio;
-  }
-
-  _getNavigationHeight(viewportHeight = null) {
-    viewportHeight = viewportHeight || this._getViewportHeight();
-    const calculatedHeight = Math.ceil(viewportHeight * 0.1);
-    const minHeight = 30 * window.devicePixelRatio;
-    return calculatedHeight > minHeight ? calculatedHeight : minHeight;
-  }
-
-  _getContentHeight() {
-    const viewportHeight = this._getViewportHeight();
-    return viewportHeight - this._getNavigationHeight(viewportHeight);
-  }
-
   attachEvents() {
-    const navigationFormEl = document.querySelector('.navigation');
-    const navigationUrlEl = document.querySelector('.navigation__url');
-
-    navigationFormEl.addEventListener('submit', () => {
-      this.browser.loadUrl(navigationUrlEl.value);
-      navigationUrlEl.blur();
-    });
-
-    navigationUrlEl.addEventListener('focus', () => {
-      this.browser.setFocusable(false);
-    });
-
-    navigationUrlEl.addEventListener('blur', () => {
-      this.browser.setFocusable(true);
-    });
-
-    window.addEventListener('resize', event => {
-      this.browser.resize(this._getContentHeight());
-    });
   }
 }
 
---
+++ www/js/navigation.js
@@ -0,0 +1,91 @@
+class BB10BrowserNavigation {
+  constructor(url) {
+    this.attachEvents();
+    this.openUrl(url);
+  }
+
+  openUrl(url) {
+    // this.updateUrl(url);
+    this.loadUrl = null;
+    navigation.openUrl(url);
+  }
+
+  updateUrl(url) {
+    if (url === this.loadUrl) {
+      return;
+    }
+
+    const navigationUrlEl = document.querySelector('.navigation__url');
+    navigationUrlEl.value = url;
+  }
+
+  attachEvents() {
+    const navigationFormEl = document.querySelector('.navigation');
+    const navigationUrlEl = document.querySelector('.navigation__url');
+
+    navigationFormEl.addEventListener('submit', () => {
+      this.openUrl(navigationUrlEl.value);
+      navigationUrlEl.blur();
+    });
+
+    navigationUrlEl.addEventListener('focus', () => {
+      console.log('select!');
+      navigationUrlEl.setSelectionRange(0, navigationUrlEl.value.length);
+    });
+
+    navigationUrlEl.addEventListener('blur', () => {
+      console.log('deselect!');
+      navigationUrlEl.setSelectionRange(0, 0);
+    });
+
+    window.onNavigationEvent = this.onNavigationEvent.bind(this);
+  }
+
+  onNavigationEvent(event) {
+    switch(event && event.type) {
+      case 'loadstart':
+        this.setLoadInProgress(true, event.url, event.navigationUrl);
+        break;
+      case 'loadstop':
+        this.setLoadInProgress(false, event.url, event.navigationUrl);
+        break;
+      case 'loadprogress':
+        this.setLoadProgress(event.progress);
+        break;
+      default:
+        console.log('unhandled event:', event);
+    }
+  }
+
+  setLoadInProgress(isInProgress, url, navigationUrl) {
+    if (!url || (this.loadUrl && url !== navigationUrl)) {
+        return;
+    }
+
+    this.loadInProgress = isInProgress;
+    this.updateUrl(url);
+    this.loadUrl = url;
+
+    this.renderProgress();
+  }
+
+  setLoadProgress(progress) {
+    this.loadProgress = parseInt(progress, 10);
+    this.renderProgress();
+  }
+
+  renderProgress() {
+    const navigationIndicatorEl = document.querySelector('.navigation__indicator');
+
+    if (!this.loadInProgress && this.loadProgress === 100) {
+      navigationIndicatorEl.style.display = 'none';
+      return;
+    }
+
+    const navigationProgressEl = document.querySelector('.navigation__progress');
+    navigationProgressEl.style.width = this.loadProgress + '%';
+    navigationIndicatorEl.style.display = 'block';
+  }
+}
+
+new BB10BrowserNavigation('https://google.com');
