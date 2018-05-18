e83bb61b752e85a060406404efa0422cc80969ea
Jonathan Reisdorf <fishlevel@crossnode.org>
2018-05-18 09:53:25 +0100

    render tabs and add events

--- www/js/tabs.js
+++ www/js/tabs.js
@@ -1,11 +1,58 @@
 class BB10BrowserTabs {
   constructor() {
-    tabs.render = this.renderTabs;
+    tabs.render = this.renderTabs.bind(this);
     tabs.getTabs('tabs.render');
   }
 
   renderTabs(tabsArray) {
-    console.log(tabsArray);
+    const tabsContainer = document.querySelector('.js-tabs');
+    const tabTemplate = document.querySelector('.js-tabTemplate').innerHTML;
+    const addTabTemplate = document.querySelector('.js-addTabTemplate').innerHTML;
+
+    const html = (tabsArray || []).map((tab, index) => {
+      return tabTemplate.replace('{{index}}', index)
+        .replace('{{title}}', tab.navigationTitle || '')
+        .replace('{{url}}', tab.navigationUrl || tab.navigationOriginalUrl || '');
+    }).join('') + addTabTemplate;
+
+    tabsContainer.innerHTML = html;
+    this.attachEvents(tabsContainer);
+  }
+
+  attachEvents(tabsContainer) {
+    const tabElements = tabsContainer.querySelectorAll('.js-tab');
+    const closeElements = tabsContainer.querySelectorAll('.js-closeTab');
+    const addTabElement = tabsContainer.querySelector('.js-addTab');
+
+    [...tabElements].forEach(tabEl => {
+      tabEl.addEventListener('click', this.openTab.bind(this));
+    });
+
+    [...closeElements].forEach(closeEl => {
+      closeEl.addEventListener('click', this.closeTab.bind(this));
+    });
+
+    addTabElement.addEventListener('click', this.addTab.bind(this));
+  }
+
+  openTab(event) {
+    const index = this._getTabByEvent(event).dataset.index;
+    console.log('openTab', index);
+  }
+
+  closeTab(event) {
+    event.stopPropagation();
+
+    const index = this._getTabByEvent(event).dataset.index;
+    console.log('closeTab', index);
+  }
+
+  addTab(event) {
+    console.log('addTab');
+  }
+
+  _getTabByEvent(event) {
+    return event.path.find(el => el.className.includes('js-tab'));
   }
 }
 
