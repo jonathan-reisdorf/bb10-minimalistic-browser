d632e0e8a21aa68cf40eec68a05a7a12cf784293
Jonathan Reisdorf <fishlevel@crossnode.org>
2018-05-23 14:27:46 +0100

    add splash

---
+++ app/src/main/java/org/crossnode/bb10beol/SplashActivity.java
@@ -0,0 +1,16 @@
+package org.crossnode.bb10beol;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
